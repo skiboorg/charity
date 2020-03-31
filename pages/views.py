728871c3b509401cd6451919e5ca4f0f4128428b
ddnnss <ddnnss.i1@gmail.com>
2020-03-31 19:05:05 +0300

    html fixes

--- pages/views.py
+++ pages/views.py
@@ -268,7 +268,7 @@ def payment(request):
                             f'returnUrl={settings.SBER_SUCCESS_URL}&'
                             f'failUrl={settings.SBER_FAIL_URL}&'
                             'pageView=DESKTOP&sessionTimeoutSecs=1200&taxSystem=1&'
-                            'additionalOfdParams":{"agent_info.type":"3"}&'
+                            'additionalOfdParams":{"name":"agent_info.type", "value":"3"}&'
                             'orderBundle={'
                             '"cartItems":{'
                             '"items":['
